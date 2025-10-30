203ec549b9e595ce2f0aa46ef5cce8520ccedcc3
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-10-30 19:19:42 +0500

    feat: update privacy mode popup visibility for larger screens

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -84,7 +84,7 @@ const SearchBox = () => {
           </div>
 
           {showPopup && (
-            <div className="absolute top-full mt-2 text-[15px] -left-11 bg-charcoal-500 text-[#404040] bg-[#FAFAF7] text-xs px-3 py-2 rounded-lg shadow-lg whitespace-nowrap">
+            <div className="hidden lg:block absolute top-full mt-2 text-[15px] -left-11 bg-charcoal-500 text-[#404040] bg-[#FAFAF7] text-xs px-3 py-2 rounded-lg shadow-lg whitespace-nowrap">
               Your project won't be used for training
               <div className="absolute bottom-full right-4 -mb-1 border-4 border-transparent border-b-charcoal-500" />
             </div>
